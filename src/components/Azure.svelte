112ac6b6682de0414991500302001ef83e9ca5e7
Authentic10 <ahmedtchagnaou@gmail.com>
2021-04-02 11:33:41 +0200

    Api consuming

--- src/components/Azure.svelte
+++ src/components/Azure.svelte
@@ -4,6 +4,8 @@
   let fileName = 'No file uploaded';
   let fileURL = '';
   let isThereAFile = false;
+  let predButton = true;
+  let prediction;
 
   function tabToggle(){
     imageTab = !imageTab;
@@ -32,6 +34,65 @@
     }
   }
     
+}
+
+function changeFile(){
+    let userFile = document.getElementById('change-file');
+    let predClasses = document.getElementById('pred-classes');
+    
+    predClasses.style.display = 'none';
+    let reader = new FileReader();
+    if('files' in userFile){
+      //alert(userFile.files.length);
+      if (userFile.files.length > 0) {
+
+        let fileSize = userFile.files[0].size;
+
+        if(fileSize > 4000000){
+          alert('File size can\'t be greater than 4 MB');
+        } else {
+          fileName = userFile.files[0].name;
+          //isThereAFile = !isThereAFile;
+          predButton = !predButton;
+          reader.onload = () => {
+            fileURL = reader.result;
+          }
+        reader.readAsDataURL(userFile.files[0]);
+        }
+    }
+  }
+    
+}
+
+async function getPrediction(){
+  const blob = await (await fetch(fileURL)).blob();
+  const response = await fetch('https://fruits-and-vegetables.cognitiveservices.azure.com/customvision/v3.0/Prediction/98fcb0c4-6c97-4607-bf60-773b6f97359f/classify/iterations/Iteration1/image', {
+    method: "POST",
+    body: blob,
+    headers: {
+              "Prediction-Key": "6d29ca369cf849de8821ed35cb1f0867",
+              "Content-Type": "application/octet-stream"
+            }
+  });
+
+  let predict = await response.json();
+
+  if(response.ok){
+    predButton = !predButton;
+    let predClasses = document.getElementById('pred-classes');  
+    if(predClasses != null) {
+      predClasses.style.display = 'block';
+    }
+    return predict
+  } else {
+    throw new Error(predict)
+  }
+
+}
+
+
+function submitHandler(e){
+  prediction = getPrediction();
 }
 
 </script>
@@ -67,9 +128,6 @@
         <label class="file-label">
           <input id="user-file" class="file-input" type="file" name="user-file" accept=".jpg, .png, .jpeg" on:change="{displayFile}">
           <span class="file-cta">
-            <!--<span class="file-icon ml-6">
-              <i class="fa fa-upload"></i>
-            </span>-->
             <span class="file-label">
               Choose a file…
             </span>
@@ -84,11 +142,42 @@
       <div class="img">
         <img src="{fileURL}" alt="File to predict">
       </div>
-      <div class="field mt-2">
-        <div class="control">
-          <button class="button is-primary">Predict</button>
+        {#if predButton}
+        <div class="field mt-2">
+          <div class="control">
+            <button class="button is-primary" on:click|preventDefault="{submitHandler}">Predict</button>
+          </div>
         </div>
+        {:else}
+        <div class="file is-centered is-boxed is-primary has-name mb-2">
+          <label class="file-label">
+            <input id="change-file" class="file-input" type="file" name="user-file" accept=".jpg, .png, .jpeg" on:change="{changeFile}">
+            <span class="file-cta">
+              <span class="file-label">
+                Change file
+              </span>
+            </span>
+            <span class="file-name">
+              {fileName}
+            </span>
+          </label>
+        </div>
+        {/if}
+      {/if}
+      {#if prediction === undefined}
+        <p></p>
+      {:else}
+      {#await prediction}
+        <p>Loading ...</p>
+      {:then value } 
+      <div id="pred-classes" >
+        {#each value.predictions as pred}
+          <p>{pred.tagName} : {pred.probability}</p> 
+        {/each}
       </div>
+      {:catch error}
+      {error.message}
+      {/await}
       {/if}
     </div>
     {/if}
@@ -126,7 +215,7 @@
         </p>
         <div class="field">
           <div class="control">
-            <button class="button is-primary">Submit</button>
+            <button class="button is-primary">Predict</button>
           </div>
         </div>
       </div>
